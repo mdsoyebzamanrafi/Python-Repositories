b9565169bf2b5fa08825f992ed19405c646fa4d3
Md. Soyeb Zaman Rafi <raficonnectpro@gmail.com>
2024-06-11 16:28:17 +0600

    stdev

--- CSE220/Assignment 1/CSE220_Assignment1_Bonus_Answer.py
+++ CSE220/Assignment 1/CSE220_Assignment1_Bonus_Answer.py
@@ -1,2 +1,16 @@
 import fhm_unittest as unittest
 import numpy as np
+x=np.array([10,8,13,9,14,25,-5,20,7,7,4])
+sum=0
+count=0
+stsum=0
+for i in range(len(x)):
+    sum+=x[i]
+    count+=1
+mean=sum/count
+for i in range(len(x)):
+    stsum+=(x[i]-mean)**2
+var=stsum/count
+stdev=var**0.5
+print(mean)
+print(stdev)
